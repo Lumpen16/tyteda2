e5368a02da4c5ec213087b2693920c03d02766de
Lumpen16 <wladozzzz@mail.ru>
2022-03-23 19:13:34 +0300

    Clients minorly styled

--- pages/components/Clients.js
+++ pages/components/Clients.js
@@ -2,8 +2,8 @@ import styles from './Clients.module.css';
 
 function Clients() {
     return (
-        <div className={ styles.clients.container}>
-            <h1>Наши клиенты</h1>
+        <div className={ styles.clients_container }>
+            <h1 className={ styles.clients_header }>Наши клиенты</h1>
             <div></div>
         </div>
     )

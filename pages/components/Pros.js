e2daf03a8f83c44091013aad1e3dd1f1793562cc
Lumpen16 <wladozzzz@mail.ru>
2022-03-23 16:47:50 +0300

    Cons styled

--- pages/components/Pros.js
+++ pages/components/Pros.js
@@ -1,14 +1,18 @@
-
+import styles from './Pros.module.css'
 
 function Pros() {
     return (
-        <div>
-            <h1>Преимущества работы с нами</h1>
-            <div>
-                <div>Зона доставки Москва и Московская область</div>
-                <div>Бесплатно доставим холодильник и СВЧ-печь</div>
-                <div>Доставим заказ <span>24/7</span> в установленное время</div>
-                <div>Заключаем договор</div>
+        <div className={ styles.pros_master_container}>
+            <div className={ styles.pros_container }>
+                <h1 className={ styles.pros_header }>Преимущества работы с нами</h1>
+                <div className={ styles.pros_list }>
+                    <div className={ styles.pros_item }>Зона доставки Москва и Московская область</div>
+                    <div className={ styles.pros_item }>Бесплатно доставим холодильник и СВЧ-печь</div>
+                    <div className={ styles.pros_item }>
+                        <div>Доставим заказ <span>24/7</span> в установленное время</div>
+                    </div>
+                    <div className={ styles.pros_item }>Заключаем договор</div>
+                </div>
             </div>
         </div>
     )

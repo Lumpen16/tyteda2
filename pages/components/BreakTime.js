dcf8b8763dbb0454b0a09952eaa2c2fe379b0145
Lumpen16 <wladozzzz@mail.ru>
2022-03-23 18:34:08 +0300

    Trusted minorly styled

--- pages/components/BreakTime.js
+++ pages/components/BreakTime.js
@@ -1,11 +1,13 @@
-
+import styles from './BreakTime.module.css'
 
 function BreakTime() {
     return (
-        <div>
-            <h1>Сделай перерыв</h1>
-            <h3>Бесплатная дегустация перед заключением договора</h3>
-            <button>Заказать</button>
+        <div className={ styles.breaktime_master_container }>
+            <div className={ styles.breaktime_container }>
+                <h1 className={ styles.breaktime_header }>Сделай перерыв</h1>
+                <h3 className={ styles.breaktime_header3 }>Бесплатная дегустация перед заключением договора</h3>
+                <button className={ styles.breaktime_button}>Заказать</button>
+            </div>
         </div>
     )
 }
--- pages/components/Trusted.js
+++ pages/components/Trusted.js
@@ -1,16 +1,20 @@
-
+import styles from './Trusted.module.css'
 
 export default function Trusted() {
     return (
         <div>
-            <h1>Нам доверяют</h1>
-            <ul>
-                <li>8 лет на рынке</li>
-                <li>Собственное производство</li>
-                <li>контроль качества</li>
-                <li>Собственный транспорт</li>
-                <li>Гибкая ценовая политика</li>
-            </ul>
+            <div className={ styles.trusted_container }>
+                <h1 className={ styles.trusted_header }>Нам доверяют</h1>
+                <div className={ styles.trusted_list_container }>
+                    <ul>
+                        <li className={ styles.trusted_list_item }>8 лет на рынке</li>
+                        <li className={ styles.trusted_list_item }>Собственное производство</li>
+                        <li className={ styles.trusted_list_item }>контроль качества</li>
+                        <li className={ styles.trusted_list_item }>Собственный транспорт</li>
+                        <li className={ styles.trusted_list_item }>Гибкая ценовая политика</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     ) 
 }

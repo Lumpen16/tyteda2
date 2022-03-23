3cd591ae4507baa4645f6a8ada556c1743123fc1
Lumpen16 <wladozzzz@mail.ru>
2022-03-23 15:56:09 +0300

    Hero section styled

--- pages/components/Hero.js
+++ pages/components/Hero.js
@@ -3,17 +3,17 @@ import styles from './Hero.module.css'
 function Hero() {
     return (
         <div>
-            <div>
-                <h1>
-                    Корпоративное питание от <span>TYTEDA.БИЗНЕС</span>
+            <div className={ styles.hero_container }>
+                <h1 className={ styles.hero_header }>
+                    Корпоративное питание от <span className={ styles.hero_buisiness}>TYTEDA.БИЗНЕС</span>
                 </h1>
                 <img></img>
-                <ul>
-                    <li>Индивидуальный подход</li>
-                    <li>Доставка 24/7</li>
-                    <li>Бесплатный дегустационный набор перед заключением договора</li>
+                <ul className={ styles.hero_buisiness}>
+                    <li className={ styles.hero_list_item }>Индивидуальный подход</li>
+                    <li className={ styles.hero_list_item }>Доставка 24/7</li>
+                    <li className={ styles.hero_list_item }>Бесплатный дегустационный набор перед заключением договора</li>
                 </ul>
-                <button>Заказать</button>
+                <button className={ styles.hero_button }>Заказать</button>
             </div>
         </div>
     )
